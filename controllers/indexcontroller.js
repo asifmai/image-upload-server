98743386909eb09e20adc8d46b7417ec3f5dea4f
Iqbal <asifmai@hotmail.com>
2020-11-17 13:27:52 +0500

    'update'

--- controllers/indexcontroller.js
+++ controllers/indexcontroller.js
@@ -1,15 +1,22 @@
 const path = require('path');
+const ip = require('ip');
 const uuid = require('uuid').v1;
 const fs = require('fs');
 const Image = require('../models/Image');
 
 module.exports.index_get = async (req, res, next) => {
   const images = await Image.find().sort({createdAt: 'desc'});
+
   res.render('index', {images});
 }
 
 module.exports.images_get = async (req, res) => {
-  const images = await Image.find().sort({createdAt: 'desc'});
+  const imagesRaw = await Image.find().sort({createdAt: 'desc'});
+  const images = imagesRaw.map(img => ({
+    id: img._id,
+    createdAt: img.createdAt,
+    fileName: 'http://' + ip.address() + `:3000/img/${img.fileName}`,
+  }));
   res.status(200).json({images});
 }
 
